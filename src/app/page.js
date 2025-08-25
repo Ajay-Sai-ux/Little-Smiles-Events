6a8e769e8e9d3501fdda9a0a395fcb651372860f
mac <mac@MacBook-Pro-mac.local>
2025-08-25 17:22:28 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,91 +5,32 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.frameParent}>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameGroup}>
+              <div className={styles.littleSmilesEventWrapper}>
+                <div className={styles.littleSmilesEvent}>Little Smiles Event</div>
+              </div>
+              <div className={styles.frameContainer}>
+                <div className={styles.comingSoonParent}>
+                  <div className={styles.comingSoon}>Coming Soon</div>
+                  <div className={styles.wereCraftingSomethingContainer}>
+                    <p className={styles.wereCraftingSomething}>We’re crafting something special!</p>
+                    <p className={styles.wereCraftingSomething}>At Little Smiles Event, we create unique, personalised return gifts that make your celebrations unforgettable. Whether it’s a birthday, wedding, baby shower, or corporate event, our handcrafted gifts are designed to spread smiles and lasting memories.</p>
+                  </div>
+                </div>
+                <div className={styles.instagramParent}>
+                  <div className={styles.instagram}>Instagram</div>
+                  <div className={styles.instagram}>Email</div>
+                  <div className={styles.instagram}>Phone</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Image className={styles.image1Icon} width={2611.9} height={579} sizes="100vw" alt="" src="/image 1.png" />
         </div>
+        
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
